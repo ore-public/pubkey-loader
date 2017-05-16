136f503892bc546d7c50aeba61fc21c6d469ad9c
Masaya Konishi <konishi@office-ore.net>
2017-05-16 21:53:06 +0900

    I tried outputting to syslog.

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <syslog.h>
 
 int main(void) {
-  printf("Hello, World\n");
+  openlog("pubkey-loader", LOG_PID, LOG_USER);
+  syslog(LOG_NOTICE, "test");
+  closelog();
 
   return 0;
 }
